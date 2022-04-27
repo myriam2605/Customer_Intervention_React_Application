6442b1bc1ee9e740e0b5e0b8c87b436817e4f056
myriam2605 <amara.myriam@hotmail.com>
2022-04-27 15:57:36 -0400

    call API to d___- first page - test-ok

--- customer_intervention_react_application/src/App.js
+++ customer_intervention_react_application/src/App.js
@@ -8,12 +8,22 @@ function App() {
     };
     const [user, setUser] = useState({ name: "", email: "" });
     const [error, setError] = useState("");
-
     const Login = (details) => {
         console.log(details);
+
+        if (details.email == adminUser.email && details.password == adminUser.password) {
+            console.log("Logged in");
+            setUser({
+                name: details.name,
+                email: details.email,
+            });
+        } else {
+            console.log("Details do not match!");
+            setError("Details do not match!");
+        }
     };
     const Logout = () => {
-        console.log("Logout");
+        setUser({ name: "", email: "" });
     };
 
     return (
@@ -23,13 +33,13 @@ function App() {
                     <h2>
                         Welcome, <span>{user.name}</span>
                     </h2>
-                    <button>Logout</button>
+                    <button onClick={Logout}>Logout</button>
                 </div>
             ) : (
-                <LoginForm />
+                <LoginForm Login={Login} error={error} />
             )}
         </div>
     );
 }
 
-export default LoginForm;
+export default App;
