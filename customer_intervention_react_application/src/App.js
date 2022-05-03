5f9fe014cf36ff903cc2d4bd98534f21e639f28d
myriam2605 <amara.myriam@hotmail.com>
2022-05-03 11:33:08 -0400

    deploy try

--- customer_intervention_react_application/src/App.js
+++ customer_intervention_react_application/src/App.js
@@ -38,20 +38,7 @@ const App = () => {
                             Home
                         </Link>
                     </li>
-                    {/* {showModeratorBoard && (
-                        <li className="nav-item">
-                            <Link to={"/mod"} className="nav-link">
-                                Moderator Board
-                            </Link>
-                        </li>
-                    )}
-                    {showAdminBoard && (
-                        <li className="nav-item">
-                            <Link to={"/admin"} className="nav-link">
-                                Admin Board
-                            </Link>
-                        </li>
-                    )} */}
+
                     {currentUser && (
                         <li className="nav-item">
                             <Link to={"/user"} className="nav-link">
